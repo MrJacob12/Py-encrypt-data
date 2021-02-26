8ed6d74ad0a59480a47ac50a7f08d87543e5ba05
MrJacob12 <anubis122003@gmail.com>
2021-02-26 09:19:40 +0100

    main

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 from cryptography.fernet import Fernet
 import glob, os
 
-def generate_key(file):
+def generate_key():
     key = Fernet.generate_key()
-    file += ".key"
-    a = "key/"
-    a += file
-    with open(a,"wb") as key_file:
-        key_file.write(key)
+    print(key)
 
 def encrypt_data(filename,keyname):
-    wxs = "key/"
-    wxs += keyname
-    wxs += ".key"
-    key = open(wxs,"rb").read()
+    key = keyname
     f = Fernet(key)
     with open(filename, "rb") as file:
         file_data = file.read()
@@ -23,10 +16,7 @@ def encrypt_data(filename,keyname):
             print("Successful encrypt data")
 
 def decrypt_data(filename,keyname):
-    wxs = "key/"
-    wxs += keyname
-    wxs += ".key"
-    key = open(wxs,"rb").read()
+    key = keyname
     f = Fernet(key)
     with open(filename, "rb") as file:
         encrypted_data = file.read()
@@ -36,25 +26,3 @@ def decrypt_data(filename,keyname):
         print("Successful decrypt data")
 
 
-while True:
-    print("List of your keys: ")
-    for file in os.listdir("key/"):
-        if file.endswith(".key"):
-            print(file)
-    try:
-        menu = input("\nEncrypt data [E] Decrypt data [D] Generate new key [K]\n")
-        if menu.lower() == "e":
-            print("Encrypt data \n")
-            filename = input("File path: ")
-            keyname = input("Key name: ")
-            encrypt_data(filename,keyname)
-        elif menu.lower() == "d":
-            print("Decrypt data \n")
-            filename = input("File path: ")
-            keyname = input("Key name: ")
-            decrypt_data(filename,keyname)
-        elif menu.lower() == "k":
-            filename = input("Filename: ")
-            generate_key(filename)
-    except:
-        print("Something went wrong")
